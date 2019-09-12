43b3ce604728e0c34649e05cac1c73fe8d2f2b9f
cjb <cjb@10.51.206.130>
2019-09-12 11:47:26 -0400

    Initial setup for BlazeDemoTest.java

--- test/BlazeDemoTest.java
+++ test/BlazeDemoTest.java
@@ -4,19 +4,43 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-public class BlazeDemoTest {
+public class BlazeDemoTest
+{
+
+	// Names 
+	//    departure select: fromPort
+	//    destination select: toPort
+	// Classes
+	//    find flights: btn
 
 	@Before
-	public void setUp() throws Exception {
+	public void setUp() throws Exception
+	{
+		
 	}
 
 	@After
-	public void tearDown() throws Exception {
+	public void tearDown() throws Exception
+	{
+		
 	}
 
 	@Test
-	public void test() {
-		fail("Not yet implemented");
+	public void Test_TC1_DepartureCities()
+	{
+		
+	}
+
+	@Test
+	public void Test_TC2_VirginAmericaFligthNumber12Details()
+	{
+		
+	}
+
+	@Test
+	public void Test_TC3_PurchaseConfirmationPage()
+	{
+		
 	}
 
 }
