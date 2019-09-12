15689efe775ddffc49c44bb2af77457d39a976ca
cjb <cjb@10.51.206.130>
2019-09-12 12:35:58 -0400

    Fix assert methods

--- test/SnakeTest.java
+++ test/SnakeTest.java
@@ -24,8 +24,8 @@ public class SnakeTest
 	public void Test_TC1_TestIsHealthFunction()
 	{
 		// Looking at isHealthy function, a snake is healthy when its favorite food is vegetables
-		assertEquals(true, takis.isHealthy());
-		assertEquals(false, peter.isHealthy());
+		assertTrue(takis.isHealthy());
+		assertFalse(peter.isHealthy());
 	}
 
 	@Test
@@ -33,14 +33,14 @@ public class SnakeTest
 	{
 		// Looking at fitsInCage, it receive a param which is the cage lenght, this test should be correct if
 		//    the snake's lenght is less than cage's length ONLY
-		assertEquals(true, peter.fitsInCage(11));
-		assertEquals(true, takis.fitsInCage(81));
+		assertTrue(peter.fitsInCage(11));
+		assertTrue(takis.fitsInCage(81));
 		
-		assertEquals(false, peter.fitsInCage(10));
-		assertEquals(false, takis.fitsInCage(80));
+		assertFalse(peter.fitsInCage(10));
+		assertFalse(takis.fitsInCage(80));
 
-		assertEquals(false, peter.fitsInCage(9));
-		assertEquals(false, takis.fitsInCage(79));
+		assertFalse(peter.fitsInCage(9));
+		assertFalse(takis.fitsInCage(79));
 	}
 
 }
